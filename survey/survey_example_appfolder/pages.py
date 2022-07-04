10afbc0ddfecd1c82235fa170cf669f64fb7e9b7
malaugonzalezp <malaugonzalezp@gmail.com>
2022-07-04 18:11:54 +0200

    adding more changes

--- survey/survey_example_appfolder/pages.py
+++ survey/survey_example_appfolder/pages.py
@@ -25,28 +25,36 @@ class Ballot2(Page):
         return {'ba_group_assignment': safe_json(self.player.ba_group_assignment)}
 
 
-#group 4 will get ideology page before ballot
+# group 0 will get ideology page before ballot
 
 class Ideology1(Page):
     form_model = Player
-    form_fields = ['ba_positioning', 'ba_party_pos']
+    form_fields = ['ba_positioning', 'ba_party_pos', 'ba_opinion1', 'ba_opinion2', 'ba_opinion3',
+                   'ba_opinion4', 'ba_opinion5', 'ba_women1', 'ba_women2', 'ba_women3', 'ba_women4', 'ba_women5',
+                   'ba_women6', 'ba_women7', 'ba_women8', 'ba_women9', 'ba_women10', 'ba_men1', 'ba_men2', 'ba_men3',
+                   'ba_women4', 'ba_women5', 'ba_men6', 'ba_men7', 'ba_men8', 'ba_men9', 'ba_men10']
 
     def is_displayed(self):
         return self.player.ba_ideology_assignment == 0
     
 
-#group 5 will get ideology page after ballot
+# group 1 will get ideology page after ballot
 
 class Ideology2(Page):
     form_model = Player
-    form_fields = ['ba_positioning', 'ba_party_pos']
+    form_fields = ['ba_positioning', 'ba_party_pos', 'ba_opinion1', 'ba_opinion2', 'ba_opinion3',
+                   'ba_opinion4', 'ba_opinion5', 'ba_women1', 'ba_women2', 'ba_women3', 'ba_women4', 'ba_women5',
+                   'ba_women6', 'ba_women7', 'ba_women8', 'ba_women9', 'ba_women10', 'ba_men1', 'ba_men2', 'ba_men3',
+                   'ba_women4', 'ba_women5', 'ba_men6', 'ba_men7', 'ba_men8', 'ba_men9', 'ba_men10']
 
     def is_displayed(self):
         return self.player.ba_ideology_assignment == 1
 
 class DemoPage(Page):
     form_model = Player
-    form_fields = ['please_state_your_age', 'ba_education', 'please_state_your_postal_code', 'ba_gender', 'ba_hidden_input']
+    form_fields = ['party_preference', 'household_income', 'eligibility', 'participation', 'social_media',
+                   'In_welchem_Land_sind_Sie_geboren', 'In_welchem_Land_ist_Ihre_Mutter_geboren',
+                   'In_welchem_Land_ist_Ihr_Vater_geboren']
 
 #change order of ballot page
 
@@ -55,9 +63,9 @@ class EndPage(Page):
     form_model = Player
 
 page_sequence = [Welcome,
+                DemoPage,
+                Ideology1,
                 Ballot1,
                 Ballot2,
-                Ideology1,
                 Ideology2,
-                DemoPage,
                 EndPage]
